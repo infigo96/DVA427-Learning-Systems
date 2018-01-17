422caca9a211979ccca92ed200046b0cc5d1da0f
FreddeG <fredrik.ano@gmail.com>
2018-01-17 14:26:35 +0100

    math

--- Assignment1/dva427lab1/main.py
+++ Assignment1/dva427lab1/main.py
@@ -1,7 +1,12 @@
 import numpy as np
+import math
 from sympy import *
 
-
+def sigmoid(x):
+  return 1 / (1 + math.exp(-x))
 
 data = np.genfromtxt('assignment 1 titanic.dat', delimiter=',')
 print(data)
+#data[1,:] get first row
+
+print(sigmoid(1))
