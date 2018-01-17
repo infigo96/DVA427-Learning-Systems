2bd22172c2580b8698eed695b6558185d4ae326d
FreddeG <fredrik.ano@gmail.com>
2018-01-17 16:42:07 +0100

    en epoch

--- Assignment1/dva427lab1/main.py
+++ Assignment1/dva427lab1/main.py
@@ -2,7 +2,7 @@ import numpy as np
 import math
 from sympy import *
 
-
+correct = 0
 np.random.seed(1)
 weights = 2* np.random.random_sample((1, 4)) - 1
 print(weights)
@@ -11,17 +11,33 @@ def sigmoid(x):
   return 1 / (1 + math.exp(-x))
 
 data = np.genfromtxt('assignment 1 titanic.dat', delimiter=',')
-#np.c_[ da, np.ones(N) ]
+data = np.c_[ data, np.ones(len(data))]
+data[:,4] = data[:,3]
+data[:,3] = 1
 print(data)
 #data[1,:] get first row
-for i in range(0,1):
-#O0 = sigmoid(data[0,0]*weights[0,0]+data[0,1]*weights[0,1]+data[0,2]*weights[0,2]+ weights[0,3])
-    o1 = sigmoid(np.dot(weights[0,0:3],data[i,0:3]) + weights[0,3])
-    delta = o1*(1-o1)*(0.5 + 0.25*data[i,3]-o1)
-    print("Fucktard  " + str(o1))
-    print("Deltatard  " + str(delta))
+for i in range(0,1500):
+    o1 = sigmoid(np.dot(weights[0,0:4],data[i,0:4]))
+    delta = o1*(1-o1)*(0.5 + 0.25*data[i,4]-o1)
 
     #for j in range (0,4):
-    weights =[0.05*delta*data[i,0:3],0.05*delta]
-    print(weights)
+    weights =weights + 0.05*delta*data[i,0:4]
+    print(i)
+print("Fucktard  " + str(o1))
+print("Deltatard  " + str(delta))
+print(weights)
+
+
+
+for i in range(1500,2200):
+    val1 = sigmoid(np.dot(weights[0, 0:4], (data[i, 0:4])))
+    if val1 >= 0.5 and data[i,4] == 1:
+        correct+=1
+    elif val1 < 0.5 and data[i, 4] == -1:
+        correct+=1
+    #print(val1)
+    #for j in range (0,4):
+
+print(correct)
+print(correct/700)
 
