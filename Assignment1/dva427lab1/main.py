6bb98d17ac53fc18601570ebac83c352ee09d4f0
FreddeG <fredrik.ano@gmail.com>
2018-01-17 16:57:11 +0100

    backup
    asdada

--- Assignment1/dva427lab1/main.py
+++ Assignment1/dva427lab1/main.py
@@ -1,4 +1,5 @@
 import numpy as np
+import time
 import math
 from sympy import *
 
@@ -10,34 +11,39 @@ print(weights)
 def sigmoid(x):
   return 1 / (1 + math.exp(-x))
 
+
 data = np.genfromtxt('assignment 1 titanic.dat', delimiter=',')
 data = np.c_[ data, np.ones(len(data))]
 data[:,4] = data[:,3]
 data[:,3] = 1
 print(data)
 #data[1,:] get first row
-for i in range(0,1500):
-    o1 = sigmoid(np.dot(weights[0,0:4],data[i,0:4]))
-    delta = o1*(1-o1)*(0.5 + 0.25*data[i,4]-o1)
+correct = 0; #initialize
+while correct/700 < 0.8:
+    for i in range(0,1500):
+        o1 = sigmoid(np.dot(weights[0,0:4],data[i,0:4]))
+        delta = o1*(1-o1)*(0.5 + 0.25*data[i,4]-o1)
 
-    #for j in range (0,4):
-    weights =weights + 0.05*delta*data[i,0:4]
-    print(i)
-print("Fucktard  " + str(o1))
-print("Deltatard  " + str(delta))
-print(weights)
+        #for j in range (0,4):
+        weights = weights - delta*data[i,0:4]
+        print(weights)
+        #print(i)
+    print("Fucktard  " + str(o1))
+    print("Deltatard  " + str(delta))
+    print(weights)
 
 
+    correct = 0
+    for i in range(1500,2201):
+        val1 = sigmoid(np.dot(weights[0, 0:4], (data[i, 0:4])))
+        if val1 >= 0.5 and data[i,4] == 1:
+            correct+=1
+        elif val1 < 0.5 and data[i, 4] == -1:
+            correct+=1
+        #print(val1)
+        #for j in range (0,4):
 
-for i in range(1500,2200):
-    val1 = sigmoid(np.dot(weights[0, 0:4], (data[i, 0:4])))
-    if val1 >= 0.5 and data[i,4] == 1:
-        correct+=1
-    elif val1 < 0.5 and data[i, 4] == -1:
-        correct+=1
-    #print(val1)
-    #for j in range (0,4):
-
-print(correct)
-print(correct/700)
+    print(correct)
+    print(correct/700)
+    time.sleep(3)
 
