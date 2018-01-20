0ba0b961b56679c3358e93feaaed903b9a1c8896
infigo96 <hbz15001@student.mdh.se>
2018-01-20 16:11:34 +0100

    Push sysPath

--- Assignment3/main.py
+++ Assignment3/main.py
@@ -1,26 +1,21 @@
 import numpy as np
+import os
 import time
 from sympy import *
 
 def NewPop(sizePop):
-    Pop = [np.random.permutation(5) for i in np.arange(3)]
+    Pop = [np.random.permutation(52) for i in np.arange(sizePop)]
     return Pop
 np.random.seed(42)
 
+abspath = os.path.abspath(__file__)
+dname = os.path.dirname(abspath)
+os.chdir(dname)
 data = np.genfromtxt('./berlin.txt', delimiter=',')
-#print(data)
 
-datanew = data[0:10][:]
-print(datanew)
 
-parent1 = np.random.permutation(10)
-parent2 = np.random.permutation(10)
-
-print('parent1 is: ' + str(parent1))
-print('parent2 is: ' + str(parent2))
-if parent1.all == parent2.all:
-    print('FUCK')
-print(NewPop(3))
+Pop = NewPop(3)
+print(Pop)
 
 #a = np.matrix('1 2 3 4; 5 6 7 8; 9 10 11 12')
 #print(a)
