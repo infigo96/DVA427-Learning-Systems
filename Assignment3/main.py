1fc6ecf165a0a4be9ce57b49631d78d7cbfd1c2a
FreddeG <fredrik.ano@gmail.com>
2018-01-20 15:52:05 +0100

    newpop

--- Assignment3/main.py
+++ Assignment3/main.py
@@ -2,5 +2,30 @@ import numpy as np
 import time
 from sympy import *
 
+def NewPop(sizePop):
+    Pop = np.zeros((3, 5))
+    for i in range (0, 3):
+        Pop[i,:] = np.random.permutation(5)
+    return Pop
+np.random.seed(42)
+
 data = np.genfromtxt('./berlin.txt', delimiter=',')
-print(data)
+#print(data)
+
+datanew = data[0:10][:]
+print(datanew)
+
+parent1 = np.random.permutation(10)
+parent2 = np.random.permutation(10)
+
+print('parent1 is: ' + str(parent1))
+print('parent2 is: ' + str(parent2))
+if parent1.all == parent2.all:
+    print('FUCK')
+print(NewPop(3))
+
+#a = np.matrix('1 2 3 4; 5 6 7 8; 9 10 11 12')
+#print(a)
+#np.random.shuffle(a)
+
+#print(a)
