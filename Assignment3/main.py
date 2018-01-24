895e4b01336664aef2cc6d5c84268dc129ef1dfc
FreddeG <fredrik.ano@gmail.com>
2018-01-24 15:48:39 +0100

    adadadsa

--- Assignment3/main.py
+++ Assignment3/main.py
@@ -6,13 +6,25 @@ from sympy import *
 def NewPop(sizePop):
     Pop = [np.random.permutation(52) for i in np.arange(sizePop)]
     return Pop
-np.random.seed(42)
+def Distance(a, b):
+    #assert data is not empty
+    return a+b
+
+def initDistanceMatrix(data):
+    return 11
+
+def routeDistance(Matrix, Individual):
+    #assert Matrix exists
+    #Individual is right size
+    #Indivudal
+    return 1
+#np.random.seed(42)
 
 abspath = os.path.abspath(__file__)
 dname = os.path.dirname(abspath)
 os.chdir(dname)
 data = np.genfromtxt('./berlin.txt', delimiter=',')
-
+A = [[Distance(i, j) for i in data for j in data]]
 
 Pop = NewPop(3)
 print(Pop)
