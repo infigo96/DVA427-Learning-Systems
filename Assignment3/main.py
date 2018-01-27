a738889fc23e0659529a6ff429308a8eefc32639
FreddeG <fredrik.ano@gmail.com>
2018-01-27 18:37:42 +0100

    routeDistance problem

--- Assignment3/main.py
+++ Assignment3/main.py
@@ -11,12 +11,21 @@ def Distance(a, b):
     return sqrt((b[0]-a[0])**2+(b[1]-a[1])**2)
 # sqrt((Y-Y0)^2+(X-X0)^2)
 
+def killWeak(Pop, numberWeak):
+    #numberweak < Pop
+    return Pop
+
+def returnBest(Pop, DistanceMatrix, numberOfBest):
+    return sorted(list(Pop), key=lambda x: routeDistance(DistanceMatrix, x))[:numberOfBest]
+
 
 def routeDistance(DistanceMatrix, Individual):
+    print(Individual)
     a = np.arange(len(Individual))
     b = a + 1
     b[len(Individual) - 1] = 0
-    return np.sum([DistanceMatrix[Individual[a[i],0]][Individual[b[i],0]]for i in np.arange(len(Individual))])
+    return 1
+    #return np.sum([DistanceMatrix[Individual[a[i],0]][Individual[b[i],0]]for i in np.arange(len(Individual))])
 
 #np.random.seed(42)
 
@@ -26,9 +35,11 @@ os.chdir(dname)
 data = np.genfromtxt('./berlin.txt', delimiter=',')
 distanceMatrix= [[Distance(i, j) for i in data] for j in data]
 #print(distanceMatrix)
-hej = routeDistance(distanceMatrix, np.transpose(NewPop(1)))
-print(hej)
+#hej = routeDistance(distanceMatrix, np.transpose(NewPop(1)))
+#print(hej)
 
+test = returnBest(NewPop(5), distanceMatrix, 1)
+#print(test)
 #Pop = NewPop(1)
 #print(Pop)
 
