bb2b7a87fcbafab9f13d391386c35f03f214a5be
infigo96 <hbz15001@student.mdh.se>
2018-02-02 17:41:08 +0100

    normalisering

--- Assignment2/lab2.m
+++ Assignment2/lab2.m
@@ -4,3 +4,4 @@ M = readtable('iris_data.csv');
 
 irisData = importfile('iris_data.csv', 1, 150);
 irisClass = M{:,5}
+irisData(:,1) = (irisData(:,1) - min(irisData(:,1)))/(max(irisData(:,1))-min(irisData(:,1)))
