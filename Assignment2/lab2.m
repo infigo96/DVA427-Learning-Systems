8c0aa2b2f0e9831a01cb1dd691d3f59dc3e79b14
FreddeG <fredrik.ano@gmail.com>
2018-02-04 14:14:54 +0100

    early access

--- Assignment2/lab2.m
+++ Assignment2/lab2.m
@@ -3,5 +3,7 @@ clc;
 M = readtable('iris_data.csv');
 
 irisData = importfile('iris_data.csv', 1, 150);
-irisClass = M{:,5}
-irisData(:,1) = (irisData(:,1) - min(irisData(:,1)))/(max(irisData(:,1))-min(irisData(:,1)))
+irisClass = M{:,5};
+for i = 1:4
+    irisData(:,i) = (irisData(:,i) - min(irisData(:,i)))/(max(irisData(:,i))-min(irisData(:,i)));
+end
