592a5ef7a097d17e85f2c42c8b3d7cd676ab6a89
FreddeG <fredrik.ano@gmail.com>
2018-02-04 15:38:44 +0100

    blah

--- Assignment2/lab2.m
+++ Assignment2/lab2.m
@@ -4,7 +4,6 @@ M = readtable('iris_data.csv');
 
 
 
-
 irisData = importfile('iris_data.csv', 1, 150);
 irisClass = M{:,5};
 for i = 1:4
@@ -13,7 +12,43 @@ end
 
 
 %short(irisData(:,1))
-test = arrayfun(@short, irisData(:,1))
+shortMatrix = arrayfun(@short, irisData);
+middleMatrix = arrayfun(@middle, irisData);
+longMatrix = arrayfun(@long, irisData);
+
+
+%test = r1(irisData(:,i))
+disp('r1 test')
+for i = 1:150
+    test = r1(irisData(i,:));
+    if test > 0
+        disp(i)
+    end
+end
+
+disp('r2 test')
+for i = 1:150
+    test = r2(irisData(i,:));
+    if test > 0
+        disp(i)
+    end
+end
+
+disp('r3 test')
+for i = 1:150
+    test = r3(irisData(i,:));
+    if test > 0
+        disp(i)
+    end
+end
+
+disp('r4 test')
+for i = 1:150
+    test = r4(irisData(i,:));
+    if test > 0
+        disp(i)
+    end
+end
 
 function y = short(x)
     if (0 <= x) && (x <0.6)
@@ -42,5 +77,43 @@ function y = middle(x)
     else
      y = 0;
     end
+end
+
+function y = r1(x)
+if (max(short(x(1)),long(x(1))) > 0.5) && (max(middle(x(2)),long(x(2))) > 0.5) && (max(middle(x(3)),long(x(3))) > 0.5) && (middle(x(4)) > 0.5) 
+    y = 1;
+else
+    y = 0;
+end
+end
+
+function y = r2(x)
+if (max(short(x(3)),middle(x(3))) > 0.5) && (short(x(4)) > 0.5) 
+    y = 1;
+else
+    y = 0;
+end
+end
+
+function y = r3(x)
+if (max(short(x(2)),middle(x(2))) > 0.5) && (long(x(3)) > 0.5) && (long(x(4)) > 0.5) 
+    y = 1;
+else
+    y = 0;
+end
+end
+
+function y = r4(x)
+if (middle(x(1)) > 0.5) &&(max(short(x(2)),middle(x(2))) > 0.5) && (short(x(3)) > 0.5) && (long(x(4)) > 0.5) 
+    y = 1;
+else
+    y = 0;
+end
+end
+
+
+
+function y = classify(x)
+ y = 0
 end
 
