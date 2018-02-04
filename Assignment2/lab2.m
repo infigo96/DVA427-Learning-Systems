71c2af0b3409556269fce8e4a1220da3ff441ac9
infigo96 <hbz15001@student.mdh.se>
2018-02-04 15:55:57 +0100

    mahshu

--- Assignment2/lab2.m
+++ Assignment2/lab2.m
@@ -1,19 +1,13 @@
 clear;
 clc;
 M = readtable('iris_data.csv');
-
-
-
-
 irisData = importfile('iris_data.csv', 1, 150);
 irisClass = M{:,5};
 for i = 1:4
     irisData(:,i) = (irisData(:,i) - min(irisData(:,i)))/(max(irisData(:,i))-min(irisData(:,i)));
 end
+hej = arrayfun(@classname, irisClass)
 
-
-%short(irisData(:,1))
-test = arrayfun(@short, irisData(:,1))
 
 function y = short(x)
     if (0 <= x) && (x <0.6)
@@ -41,6 +35,17 @@ function y = middle(x)
 
     else
      y = 0;
+    end
+end
+function y = classname(x)
+    if strcmp(x{1},'Iris-versicolor')
+        y =1;
+    elseif strcmp(x{1},'Iris-setosa')
+        y = 2;
+    elseif strcmp(x{1},'Iris-virginica')
+        y = 3;
+    else
+        y = 0
     end
 end
 
