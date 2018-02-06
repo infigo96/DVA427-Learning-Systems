db5dd2229fad5dc0c7965349f23c3de8c09995ab
FreddeG <fredrik.ano@gmail.com>
2018-02-06 16:06:09 +0100

    v1

--- Assignment4/lab4.m
+++ Assignment4/lab4.m
@@ -1,3 +1,21 @@
 clear;
 %clf;
 clc;
+
+A=importdata('cities.txt');
+A.textdata{3,1}
+C = [cell2mat(A.textdata(:,1)),cell2mat(A.textdata(:,2))]
+C = arrayfun(@uint8, C);
+locations = C - 64; 
+inputPath = [locations, A.data]; %column 1 and 2 are the location, column 3 is the distance. 
+%char(23+64) W
+distanceMatrix = ones(26,26)*inf;
+for i = 1:26
+distanceMatrix(inputPath(i,1),inputPath(i,2)) = inputPath(i,3)
+end
+
+currentDistance = [reshape( 1:26, 26, 1), inf(26,1)]
+currentDistance(6, 2) = 0;
+
+
+
