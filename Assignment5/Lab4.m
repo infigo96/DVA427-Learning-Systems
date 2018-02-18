1ed6cee36e24bbecbb7ccd47582153cf415d07c3
FreddeG <fredrik.ano@gmail.com>
2018-02-18 17:01:23 +0100

    clock update

--- Assignment5/Lab4.m
+++ Assignment5/Lab4.m
@@ -1,9 +1,11 @@
 clc;
 clear;
+clf;
 
 startState = [0 0 0 0];
+learnRate = 0.5;
 
-x1 = -pi/6:0.5:pi/6; %theta. THe angle of the pendelum.
+x1 = -pi/6:0.1:pi/6; %theta. THe angle of the pendelum.
 x2 = -pi:0.1:pi; %theta dot. The angle velocity of the pendelum
 x3 = -2.4:0.1:2.4; %x pos. The position of the cart
 x4 = -10:1:10; %x dist dot. The speed of the cart
@@ -42,12 +44,48 @@ rewardFunc = @(x1,x2,x3,x4)(-1*((abs(x1)).^2) - (0.2*(abs(x2)).^2) - (0.1*(abs(x
    %Initalize starting values
  currentState = startState;
  
+ %% Set up the pendulum plot
+panel = figure;
+panel.Position = [680 558 1000 400];
+panel.Color = [1 1 1];
+subplot(1,4,1)
+
+hold on
+ % Axis for the pendulum animation
+f = plot(0,0,'b','LineWidth',10); % Pendulum stick
+axPend = f.Parent;
+axPend.XTick = []; % No axis stuff to see
+axPend.YTick = [];
+axPend.Visible = 'off';
+axPend.Position = [0.01 0.5 0.3 0.3];
+axPend.Clipping = 'off';
+axis equal
+axis([-1.2679 1.2679 -1 1]);
+plot(0.001,0,'.k','MarkerSize',50); % Pendulum axis point
+
+hold off
+%%%%%%%%%%%%%%%%%%%%%%%%%
+ 
 
 for episode = 1:maxEpisodes
+    
+            set(f,'XData',[0 -sin(currentState(1))]);
+            set(f,'YData',[0 cos(currentState(1))]);
  
     
-   [~,stateIndex] = min(sum((states - repmat(currentState,[size(states,1),1])).^2,2));
+   [~,stateIndex] = min(sum((states - repmat(currentState,[size(states,1),1])).^2,2)); %closest state as described by our state
  
-   [~,actionIndex] = max(Q(stateIndex,:));
+   [~,actionIndex] = max(Q(stateIndex,:)); % Calculates the next action to do from Qmatrix
+   nextState = SimulatePendel(actions(actionIndex), currentState(1), currentState(2), currentState(3), currentState(4)); 
+   
+   [~,nextStateIndex] = min(sum((states - repmat(nextState,[size(states,1),1])).^2,2)); %closest state as described by our state
+   R(nextStateIndex)
+   Q(stateIndex,actionIndex) + learnRate * (R(nextStateIndex) + max(Q(nextStateIndex,:)) - Q(stateIndex,actionIndex))
+   Q(stateIndex,actionIndex) = Q(stateIndex,actionIndex) + learnRate * (R(nextStateIndex) + max(Q(nextStateIndex,:)) - Q(stateIndex,actionIndex));
+   
+   currentState = nextState;
+   pause(1)
+   
+   
 
 end  % SimulatePendel(1, 1, 1, 1, 1)
