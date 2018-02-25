67d8519955a0d9cf302b4bd44223638f247c82bc
infigo96 <hbz15001@student.mdh.se>
2018-02-25 13:04:01 +0100

    Fresha grejer

--- Assignment5/Lab5.m
+++ Assignment5/Lab5.m
@@ -1,12 +1,12 @@
 clc;
 clear;
 clf;
-
+train = 0;
 startState = [0 0 0 0];
 learnRate = 0.95;
 toPause = 0;
 
-x3 = -pi/6:0.04:pi/6; %theta. THe angle of the pendelum.
+x3 = -pi/6:0.04:pi/6; %theta. The angle of the pendelum.
 x4 = -pi:0.3:pi; %theta dot. The angle velocity of the pendelum
 x1 = -2.4:0.6:2.4; %x pos. The position of the cart
 x2 = -10:2.5:10; %x dist dot. The speed of the cart
@@ -38,7 +38,9 @@ rewardFunc = @(x1,x2,x3,x4)(-1*((abs(x3)).^2) - (0.25*(abs(x4)).^2) - (0.1*(abs(
 
  R = rewardFunc(states(:,1),states(:,2),states(:,3),states(:,4));
  Q = repmat(R,[1,2]);
- 
+ if exist('SavedQ.mat', 'file') == 2
+    load('SavedQ','Q')
+ end
    %Initalize starting values
  currentState = startState;
  
@@ -66,10 +68,14 @@ hold off
  
 
 for episode = 1:maxEpisodes
-    currentState = startState;
+    if train == 0
+        currentState = startState;
+    elseif train == 1
+        currentState = [-2.4 + (2.4+2.4)*rand, 0 ,-pi/20 + (pi/10)*rand, 0];
+    end
     index = 0;
 
-    while(abs(currentState(1)) < 2.4 && abs(currentState(3))<pi/15 && index < 40000 )
+    while(abs(currentState(1)) < 2.4 && abs(currentState(3))<pi/15 && index < 20000 )
        index = index + 1;
     
            set(f,'XData',[0 -sin(currentState(3))]);
@@ -88,36 +94,38 @@ for episode = 1:maxEpisodes
 %        end
 %    end
  
-   %[~,actionIndex] = max(Q(stateIndex,:)); % Calculates the next action to do from Qmatrix
-   nextState = SimulatePendel(actions(actionIndex), currentState(1), currentState(2), currentState(3), currentState(4)); 
-   
-   [~,nextStateIndex] = min(sum((states - repmat(nextState,[size(states,1),1])).^2,2)); %closest state as described by our state
-   %R(nextStateIndex)
-   %Q(stateIndex,actionIndex) + learnRate * (R(nextStateIndex) + max(Q(nextStateIndex,:)) - Q(stateIndex,actionIndex))
-   Q(stateIndex,actionIndex) = Q(stateIndex,actionIndex) + learnRate * (R(nextStateIndex) + 0.9*max(Q(nextStateIndex,:)) - Q(stateIndex,actionIndex));
-   
-   currentState = nextState;
-   clc;
-   disp('%%%%%%%%%%%%%%%%%%%%%%%%%%');
-   disp(episode)
-   disp('cart distance is: ');
-   disp(currentState(1));
-   disp('cart speed is: ');
-   disp(currentState(2));
-   disp('Angle is: ');
-   disp(currentState(3));
-   %disp(180/pi*currentState(3));
-   disp('Angle velocity is: ');
-   %disp(180/pi*currentState(4));
-   disp(currentState(4));
-   disp('Survival time');
-   disp(index*0.02);
-    if (toPause > 0)
-            pause(toPause)
+       %[~,actionIndex] = max(Q(stateIndex,:)); % Calculates the next action to do from Qmatrix
+       nextState = SimulatePendel(actions(actionIndex), currentState(1), currentState(2), currentState(3), currentState(4)); 
+
+       [~,nextStateIndex] = min(sum((states - repmat(nextState,[size(states,1),1])).^2,2)); %closest state as described by our state
+       %R(nextStateIndex)
+       %Q(stateIndex,actionIndex) + learnRate * (R(nextStateIndex) + max(Q(nextStateIndex,:)) - Q(stateIndex,actionIndex))
+       Q(stateIndex,actionIndex) = Q(stateIndex,actionIndex) + learnRate * (R(nextStateIndex) + 0.9*max(Q(nextStateIndex,:)) - Q(stateIndex,actionIndex));
+
+       currentState = nextState;
+       clc;
+       disp('%%%%%%%%%%%%%%%%%%%%%%%%%%');
+       disp(episode)
+       disp('cart distance is: ');
+       disp(currentState(1));
+       disp('cart speed is: ');
+       disp(currentState(2));
+       disp('Angle is: ');
+       %disp(currentState(3));
+       disp(180/pi*currentState(3));
+       disp('Angle velocity is: ');
+       %disp(180/pi*currentState(4));
+       disp(currentState(4));
+       disp('Survival time');
+       disp(index*0.02);
+        if (toPause > 0)
+                pause(toPause)
+        end
+
     end
-   
+    if (mod(episode, 100) == 0)
+        save('SavedQ','Q')
     end
-   
    
 
 end  % SimulatePendel(1, 1, 1, 1, 1)
